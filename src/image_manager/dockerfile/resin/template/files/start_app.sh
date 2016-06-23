4e531227c88e5aa4249198a5fa793e7b68c9c208
liujinliu <liujinliu@le.com>
2016-06-23 11:12:48 +0800

    delete config and dockerfile path from src

--- src/image_manager/dockerfile/resin/template/files/start_app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#start_app
-chown 644 /letv/deployment/$APP_NAME -R
-
-sh /letv/server/resin/bin/resin.sh -server $APP_NAME start
-
--- src/image_manager/dockerfile/tomcat/template/files/start_app.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#start_app
-chown 644 /letv/deployment/$APP_NAME -R
-
-/etc/init.d/tomcat_8080 start
