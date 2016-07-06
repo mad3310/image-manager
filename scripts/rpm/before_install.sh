69d7560733e728b78876baf969ef256e76108612
liujinliu <liujinliu@le.com>
2016-07-06 15:09:41 +0800

    add dependency packages

--- scripts/rpm/before_install.sh
+++ scripts/rpm/before_install.sh
@@ -11,9 +11,19 @@ if [ ! -d $VENV_NAME ]; then
 fi
 
 source ${VENV_NAME}/bin/activate
-pip install docker-py==1.8.1
-pip install tornado==4.3
-pip install futures==3.0.5
-pip install boto==2.40.0
-pip install kazoo==2.2.1
+cd /opt/letv/image-manager/packages
+pip install ordereddict-1.1.tar.gz
+pip install backports.ssl_match_hostname-3.5.0.1.tar.gz
+pip install argparse-1.4.0-py2.py3-none-any.whl
+pip install backports_abc-0.4-py2.py3-none-any.whl
+pip install ipaddress-1.0.16-py27-none-any.whl
+pip install six-1.10.0-py2.py3-none-any.whl
+pip install singledispatch-3.4.0.3-py2.py3-none-any.whl
+pip install requests-2.10.0-py2.py3-none-any.whl
+pip install futures-3.0.5-py2-none-any.whl
+pip install boto-2.40.0-py2.py3-none-any.whl
+pip install websocket_client-0.37.0.tar.gz 
+pip install docker_py-1.8.1-py2.py3-none-any.whl
+pip install kazoo-2.2.1-py2.py3-none-any.whl
+pip install tornado-4.3-cp35-none-win_amd64.whl
 exit 0
