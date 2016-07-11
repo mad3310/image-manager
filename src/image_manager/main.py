654315eb2fa510b978ee4213b06f3a64309176f1
liujinliu <liujinliu@le.com>
2016-07-11 16:12:03 +0800

    improve logging

--- src/image_manager/main.py
+++ src/image_manager/main.py
@@ -3,14 +3,17 @@ __author__ = 'xsank'
 import os
 import logging
 import logging.config
+import tornado.options
+from image_manager.appdefine import appDefine
+tornado.options.parse_command_line()
+
+from tornado.options import options
+config_path = os.path.join(options.base_dir, "config")
+logging.config.fileConfig(config_path + '/logging.conf')
 
 import tornado.web
 import tornado.ioloop
 import tornado.httpserver
-import tornado.options
-from tornado.options import options
-
-from image_manager.appdefine import appDefine
 from image_manager.routes import handlers
 from image_manager.utils import zkOpers
 
@@ -22,8 +25,6 @@ class Application(tornado.web.Application):
 
 def main():
     tornado.options.parse_command_line()
-    config_path = os.path.join(options.base_dir, "config")
-    logging.config.fileConfig(config_path + '/logging.conf')
     zkOpers.zk_handler.conn()
     http_server = tornado.httpserver.HTTPServer(Application())
     http_server.listen(options.port)
